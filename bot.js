700d107df78929fca48c4e240b4082fdbcf1987b
Matthew Starkey <matthew.starkey@outlook.com>
2021-05-27 19:48:57 -0600

    Update bot.js
    Updated code. Added shuffling and queue specs. Removed private key token from code.

--- bot.js
+++ bot.js
@@ -1,16 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('ready', () => { console.log("I'm ready!") });
-const prefix = '!';
+const request = require('request');
+client.on('ready', () => { console.log("StarkBOT online!") });
+//command prefix
+let prefix = '!';
 
 //DISCORD MUSIC BOT:
 const ytdl = require('ytdl-core');
 const ytSearch = require('yt-search');
 
+//Spotify requests:
+playlistURL = 'https://api.spotify.com/v1/playlists/';
+spotifyRequest = 'https://accounts.spotify.com/authorize';
+clientID = '427024547a9b4a218ae7988760211f8f';
+clientSecret = '4782656788ba4286a09d666f809cbce5';
+
 //used to keep track of songs, and whether bot is currently playing a song
 let songQueue = [];
 currentlyPlaying = false;
 currentSong = "Nothing";
+//IM OUT - plays link when bot leaves
+let link = 'https://www.youtube.com/watch?v=8iUFEMEySNc';
 
 client.on('message', async (message) => {
     if (!message.content.startsWith(prefix) || message.author.bot) { return; }
@@ -32,6 +42,14 @@ client.on('message', async (message) => {
             //returns top result. if no results return null
             return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
         }
+        if(search.startsWith('https://open.spotify')) {
+            let searchSplit = search.split('/');
+            console.log(searchSplit);
+            let spotifyID = searchSplit[4].split('?')[0];
+            console.log(spotifyID);
+            request(playlistURL + spotifyID)
+            return; 
+        }
         //pulls video from users arguments
         const video = await videoFinder(search);
         if (video) {
@@ -40,13 +58,15 @@ client.on('message', async (message) => {
                 let song = songQueue.shift();
                 //converts video to audio and video stream
                 currentSong = song.title + ' (' + song.duration.timestamp + ')';
-                let stream = ytdl(song.url, { filter: 'audioandvideo'});
+                let stream = ytdl(song.url, { filter: 'audioonly'});
                 connection.play(stream, {seek: 0, volume: 1})
                 .on('finish', ()=>{
                     playNextSong(connection, message, voiceChannel);
                 });
                 currentlyPlaying = true;
                 await message.channel.send('***Now Playing: ' +  currentSong + '***');
+            } else {
+                await message.channel.send('***Added ' + video.title + ' (' + video.duration.timestamp +') to queue***');
             }
         } else {
             message.channel.send("Failed to find: " + search);
@@ -63,48 +83,52 @@ client.on('message', async (message) => {
     if(command == 'skip') {
         playNextSong(connection, message, voiceChannel);
     }
+    if(command == 'setprefix') {
+        prefix = search;
+    }
+    if(command == 'shuffle') {
+        shuffleArray(songQueue, message);
+    }
+    if(command == 'queue' || command == 'q') {
+        let songList = "";
+        for(let i = 0; i < songQueue.length; i++) {
+            songList += (i + ': ' + songQueue[i].title + '\n');
+        }
+        message.channel.send(songList);
+    }
 });
 
 async function playNextSong(connection, message, voiceChannel) {
     if(songQueue.length > 0) {
         let video = songQueue.shift();
-        let stream = ytdl(video.url, { filter: 'audioandvideo'});
+        let stream = ytdl(video.url, { filter: 'audioonly'});
         currentSong = video.title + ' (' + video.duration.timestamp + ')';
         connection.play(stream, {seek: 0, volume: 1})
         .on('finish', () => { //when song finishes playing
-            playNextSong(connection, message);
+            playNextSong(connection, message, voiceChannel);
         });
         await message.channel.send('***Now Playing: ' + currentSong + '***');
     } else {
-        voiceChannel.leave();
+        //play f' this sh*t im out!
+        let video = await ytSearch(link);
+        let song = video.videos[0];
+        let imOUT = ytdl(song.url, {filter: 'audioonly'});
+        connection.play(imOUT, {seek: 0, volume: 1})
+        .on('finish', () =>{
+            voiceChannel.leave();
+        });
+        //leave voice channel
         currentlyPlaying = false;
         currentSong = "Nothing";
     }
 }
 
-const noahReplies = ['Fuck off randy!', 'cock and nuts', 'Shut up cum slut', 'stick deez nuts in ur mouf',
-    'cunt', 'stinkkyyyy', 'pussy ass bitch', 'lil sourcream lookin ass', 'whore', 'go white boy! go!'
-    , 'shut the fuck up', 'who asked?'];
-
-
-client.on('message', msg => {
-    if (msg.content == '!ping') { //replies to you with a mean message
-        let ind = Math.floor(Math.random() * noahReplies.length);
-        msg.reply(noahReplies[ind]);
+function shuffleArray(array,message) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
-});
-
-//on voicestateupdate gives a message on voice chat join
-// client.on('voiceStateUpdate', (oldState, state) => {
-//     if(state.channelID != null) {
-//         let textChannel = state.channel.guild.channels.cache.get('408499521434157056'); //accesses text channel from id
-//         let id = state.member.user.discriminator; //grabs joined members discriminator ID
-//         if(id == '0190') {
-//             textChannel.send('yeet');
-//         } else if (id == '6969') {
-//             textChannel.send('Noah...')
-//         }
-//     }
-// });
-//Outdated file token for safety purposes!
-client.login('NzgxMjA3MzI5OTI3OTg3MjMx.X76SBQ.4UUD5QUr2yWlWyjT6wbwfEond1I')
+    message.channel.send('***Shuffled Queue***');
+}
